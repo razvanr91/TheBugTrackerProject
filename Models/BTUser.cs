bae219af03388978bc07720165ae19445bba4ffb
razvanr91 <razvan.roman91@outlook.com>
2021-12-04 02:44:57 +0000

    !

---
+++ Models/BTUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace TheBugTrackerProject.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string Fullname { get; set; }
+    }
+}
